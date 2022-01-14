dfda031a2ee1bbfc7b8ef967b140eeb6a35057b0
Nooran Shah <32373343+nooranshah@users.noreply.github.com>
2022-01-14 16:47:41 +0300

    ViewChild decorator worked

--- chapter01/start_here/cc-view-child/src/app/app.component.ts
+++ chapter01/start_here/cc-view-child/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cc-view-child';
 
+  @ViewChild(GalleryComponent) gallery:GalleryComponent;
+
+ 
   addNewPicture() {
-    console.log('added new picture');
+    this.gallery.pictures.unshift(this.gallery.generateImage())
   }
 
   removeFirstPicture() {
-    console.log('removed first picture');
+    this.gallery.pictures.shift();
   }
 }
+
+
