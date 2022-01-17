4b3c89cf971ef4623cb3a033d5eed1a80c52bd21
nooranshah <32373343+nooranshah@users.noreply.github.com>
2022-01-17 23:18:08 +0300

    #cc-geteer and setted implemented

--- chapter01/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./vc-logs.component.scss']
 })
 export class VcLogsComponent implements OnInit {
-  @Input() vName;
+  _vName:string="";
+  logs:string[]=[];
+
+  @Input() get vName(){
+    return this._vName;
+  }
+
+  set vName(name:string){
+    if(!name) return;
+    if(!this._vName)
+    this.logs.push(`initial version is ${name.trim()}`)
+    this._vName =name;
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
