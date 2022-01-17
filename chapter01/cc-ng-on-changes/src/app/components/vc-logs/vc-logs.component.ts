345473cc46fb7dda198fbb03bd28e846c6b0801a
nooranshah <32373343+nooranshah@users.noreply.github.com>
2022-01-17 23:37:22 +0300

    cc-hook onchanges

--- chapter01/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss']
 })
-export class VcLogsComponent implements OnInit {
+export class VcLogsComponent implements OnInit, OnChanges {
   @Input() vName;
+  logs: string[] = []
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    const currValue = changes.vName.currentValue;
+    if (changes.vName.isFirstChange()) {
+      this.logs.push(`initial version is ${currValue.trim()}`)
+    }
+    else {
+      this.logs.push(`version changed to ${currValue.trim()}`)
+    }
   }
 
 }
