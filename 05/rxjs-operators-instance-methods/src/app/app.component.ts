46115478b61c0318086572b248985c48f1ddf137
nooranshah <32373343+nooranshah@users.noreply.github.com>
2022-03-23 23:26:07 +0300

    Working with RxJS operators using instance  methods

---
+++ 05/rxjs-operators-instance-methods/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map,merge } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  subscription: Subscription = null;
+  inputStreamData = ['john wick', 'inception', 'interstellar'];
+  cartoonsStreamData = ['thunder cats', 'Dragon Ball Z', 'Ninja Turtles'];
+  outputStreamData = [];
+
+  ngOnInit() {
+  }
+
+  startStream() {
+    const streamSource = interval(1500);
+
+    const cartoonStreamSource = interval(1000).pipe(map((output:any)=>output % this.cartoonsStreamData.length),map((index:any)=>this.cartoonsStreamData[index]));
+    this.subscription = streamSource.pipe(map((output:any)=>output % this.inputStreamData.length),map((index:any)=>this.inputStreamData[index]),merge(cartoonStreamSource)).subscribe(element => {
+      this.outputStreamData.push(element);
+    });
+  }
+
+  stopStream() {
+    this.subscription.unsubscribe();
+    this.subscription = null;
+  }
+
+
+}
